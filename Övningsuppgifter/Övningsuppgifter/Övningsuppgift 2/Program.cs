2e16e7fbd984f46d9c615b29a4dccb8032dc0700
Morten Folkesson <Morten.Folkesson@live.se>
2021-12-08 11:20:23 +0100

    f

--- Övningsuppgifter/Övningsuppgifter/Övningsuppgift 2/Program.cs
+++ Övningsuppgifter/Övningsuppgifter/Övningsuppgift 2/Program.cs
@@ -1,32 +1,57 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿#region WHAT TO IMPLEMENT
 
-#region Vad ska implementeras
+/* IDEA
 
-    /*
-     
-    MAIN MENU
+MAIN MENU
 
-    -------------------------------------------------------------------
-    Welcome admin
-    
-    Type what you would like to do
-    Add person: *Add*
-    "Type the persons name"
-        *name*
-    "Type persons age"
-        *age*
+-------------------------------------------------------------------
+Welcome admin
 
-    Remove person: *Remove*
-        *shows list*
-    "Who would you like to remove?"
-        *type name*
-    "Are you sure?"
-        *Y/N*
+Type what you would like to do
+Add person: *Add*
+"Type the persons name"
+    *name*
+"Type persons age"
+    *age*
 
-    See persons: See
-        *shows list*
-      */
+Remove person: *Remove*
+    *shows list*
+"Who would you like to remove?"
+    *type name*
+"Are you sure?"
+    *Y/N*
+
+See persons: See
+    *shows list*
+  */
 
 #endregion
+
+#region START PROGRAM
+
+//Welcomes on start of program
+{
+    Console.WriteLine("Main menu");
+    Console.WriteLine("_____________________");
+    Console.WriteLine("Welcome admin\n");
+    Console.ReadKey();
+    Console.WriteLine("Welcome admin\n");
+
+
+    Console.WriteLine("\nView List: View");
+    Console.WriteLine("Add Person: Add");
+    Console.WriteLine("Remove Person: Remove");
+
+
+    Console.ReadKey();
+}
+#endregion
+
+Console.ReadLine();
+
+
+string AddPerson = "Add";
+string RemovePerson = "Remove";
+string ViewList = "View";
+
+
