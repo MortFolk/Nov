028c394df283ba18e1e47d3340f61a0994899164
Morten Folkesson <Morten.Folkesson@live.se>
2021-12-08 09:42:23 +0100

    First draft - see if it works

--- Övningsuppgifter/Övningsuppgifter/Övningsuppgift 2/Program.cs
+++ Övningsuppgifter/Övningsuppgifter/Övningsuppgift 2/Program.cs
@@ -1,5 +1,32 @@
 ﻿
 Console.WriteLine("Hello, World!");
-
-
 Console.ReadLine();
+
+#region Vad ska implementeras
+
+    /*
+     
+    MAIN MENU
+
+    -------------------------------------------------------------------
+    Welcome admin
+    
+    Type what you would like to do
+    Add person: *Add*
+    "Type the persons name"
+        *name*
+    "Type persons age"
+        *age*
+
+    Remove person: *Remove*
+        *shows list*
+    "Who would you like to remove?"
+        *type name*
+    "Are you sure?"
+        *Y/N*
+
+    See persons: See
+        *shows list*
+      */
+
+#endregion
